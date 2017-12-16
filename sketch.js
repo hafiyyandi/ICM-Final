29c348ed4b60c0b48c8226fa48daf46fe129be03
hafiyyandi <hafiyyandi@gmail.com>
2017-12-16 15:28:34 -0500

    canvas size again

--- sketch.js
+++ sketch.js
@@ -57,7 +57,7 @@ function loadArt(songs){
 
 function setup() { 
 
-  createCanvas(1200,400);
+  createCanvas(1200,600);
   //console.log(songs.data[0].title);
   ellipseMode(CENTER);
   //ellipseMode(RADIUS);
